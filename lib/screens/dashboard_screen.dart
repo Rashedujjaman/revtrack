5cbbb461e7b9c636ca6a9af3f26a9fe7a1c0b5fe
MD Rashedujjaman Reza <68024439+Rashedujjaman@users.noreply.github.com>
2025-05-11 13:43:17 +0600

    Chart initialization

--- lib/screens/dashboard_screen.dart
+++ lib/screens/dashboard_screen.dart
@@ -1,8 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:revtrack/theme/gradient_provider.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
+import 'package:syncfusion_flutter_charts/sparkcharts.dart';
 
-class DashboardScreen extends StatelessWidget {
+class DashboardScreen extends StatefulWidget {
   const DashboardScreen({super.key});
+
+  @override
+  State<DashboardScreen> createState() => _DashboardScreenState();
+}
+
+class _DashboardScreenState extends State<DashboardScreen> {
+  final List<_SalesData> data = [
+    _SalesData('Jan', 35),
+    _SalesData('Feb', 28),
+    _SalesData('Mar', 34),
+    _SalesData('Apr', 32),
+    _SalesData('May', 40),
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -102,9 +117,57 @@ class DashboardScreen extends StatelessWidget {
                 ),
               ),
             ),
+            //Initialize the chart widget
+            SfCartesianChart(
+              primaryXAxis: CategoryAxis(),
+              // Chart title
+              title: ChartTitle(text: 'Half yearly sales analysis'),
+              // Enable legend
+              legend: Legend(isVisible: true),
+              // Enable tooltip
+              tooltipBehavior: TooltipBehavior(enable: true),
+              series: <CartesianSeries<_SalesData, String>>[
+                LineSeries<_SalesData, String>(
+                  dataSource: data,
+                  xValueMapper: (_SalesData sales, _) => sales.year,
+                  yValueMapper: (_SalesData sales, _) => sales.sales,
+                  name: 'Sales',
+                  // Enable data label
+                  dataLabelSettings: DataLabelSettings(isVisible: true),
+                ),
+              ],
+            ),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                //Initialize the spark charts widget
+                child: SfSparkLineChart.custom(
+                  //Enable the trackball
+                  trackball: SparkChartTrackball(
+                    activationMode: SparkChartActivationMode.tap,
+                  ),
+                  //Enable marker
+                  marker: SparkChartMarker(
+                    displayMode: SparkChartMarkerDisplayMode.all,
+                  ),
+                  //Enable data label
+                  labelDisplayMode: SparkChartLabelDisplayMode.all,
+                  xValueMapper: (int index) => data[index].year,
+                  yValueMapper: (int index) => data[index].sales,
+                  dataCount: 5,
+                ),
+              ),
+            ),
           ],
         )),
       ),
     );
   }
+}
+
+class _SalesData {
+  _SalesData(this.year, this.sales);
+
+  final String year;
+  final double sales;
 }
--- lib/theme/theme.dart
+++ lib/theme/theme.dart
@@ -2,20 +2,22 @@ import 'package:flutter/material.dart';
 
 ThemeData lightMode = ThemeData(
     brightness: Brightness.light,
-    colorScheme: const ColorScheme.light(
+    colorScheme: ColorScheme.light(
       background: Color(0xFF64E6F0),
       inversePrimary: Colors.white,
       primary: Colors.black,
       secondary: Colors.grey,
       tertiary: Colors.amber,
+      surfaceVariant: Colors.grey[300]!,
     ));
 
 ThemeData darkMode = ThemeData(
     brightness: Brightness.dark,
-    colorScheme: const ColorScheme.dark(
+    colorScheme: ColorScheme.dark(
       background: Colors.black,
       inversePrimary: Color(0xFF64E6F0),
       primary: Colors.white,
       secondary: Colors.grey,
       tertiary: Colors.amber,
+      surfaceVariant: Colors.grey[700]!,
     ));
